8e040b49576167fc7a3da20a030c91579a95e884
StephenGemin <s.gemin88@gmail.com>
2026-06-23 17:18:35 -0400

    small tweaks

--- src/main.c
+++ src/main.c
@@ -33,12 +33,14 @@ int main(int argc, char **argv) {
     int sub_argc = argc - 2;
     char **sub_argv = argv + 2;
 
-    if (strcmp(cmd, "add") == 0)                      return cmd_add(sub_argc, sub_argv);
-    if (strcmp(cmd, "rm") == 0 ||
-        strcmp(cmd, "remove") == 0)                   return cmd_remove(sub_argc, sub_argv);
-    if (strcmp(cmd, "ls") == 0 ||
-        strcmp(cmd, "list") == 0)                     return cmd_list(sub_argc, sub_argv);
-    if (strcmp(cmd, "search") == 0)                   return cmd_search(sub_argc, sub_argv);
+    if (strcmp(cmd, "add") == 0)                      
+        return cmd_add(sub_argc, sub_argv);
+    if (strcmp(cmd, "rm") == 0 || strcmp(cmd, "remove") == 0)
+        return cmd_remove(sub_argc, sub_argv);
+    if (strcmp(cmd, "ls") == 0 || strcmp(cmd, "list") == 0)                     
+        return cmd_list(sub_argc, sub_argv);
+    if (strcmp(cmd, "search") == 0)                   
+        return cmd_search(sub_argc, sub_argv);
 
     fprintf(stderr, "notes: unknown command '%s'\n\n", cmd);
     usage();
